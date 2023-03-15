4bc4c767d6c6bddb4af2b4a9f8349b1d8071c05b
andrerusso02 <andrerusso02@gmail.com>
2023-03-15 19:27:25 +0100

    pid réglé !

--- diffbot_base/src/test_pid.cpp
+++ diffbot_base/src/test_pid.cpp
@@ -35,20 +35,31 @@ int main(int argc, char **argv) {
 
     ros::Rate loop_rate(10);
 
+    double cmd_vel = 0;
+
+    double step = 0.4;
+
     while (ros::ok()) {
 
         diffbot_msgs::WheelsCmdStamped wheel_cmd_msg;
         wheel_cmd_msg.header.stamp = ros::Time::now();
 
 
+        // command wheel velocities between -vel and vel
+        
+        wheel_cmd_msg.wheels_cmd.angular_velocities.joint.push_back(cmd_vel);
+        wheel_cmd_msg.wheels_cmd.angular_velocities.joint.push_back(cmd_vel);
 
-        wheel_cmd_msg.wheels_cmd.angular_velocities.joint.push_back(vel);
-        wheel_cmd_msg.wheels_cmd.angular_velocities.joint.push_back(0);
+
+        cmd_vel += step;
+        if (cmd_vel > vel || cmd_vel < -vel) {
+            step = -step;
+        }
 
         pub_wheel_cmd_velocities.publish(wheel_cmd_msg);
 
         std_msgs::Float32 vel_msg;
-        vel_msg.data = vel;
+        vel_msg.data = cmd_vel;
         pub_cmd_vel_for_plot.publish(vel_msg);
 
         ros::spinOnce();
