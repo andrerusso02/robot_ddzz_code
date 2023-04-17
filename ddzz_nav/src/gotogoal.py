73c589ae48c8661b704fcce1b3858e8fcaf63c90
andrerusso02 <andrerusso02@gmail.com>
2023-04-17 20:16:03 +0200

    deplacement ok (sauf rotation etrange)

--- ddzz_nav/src/gotogoal.py
+++ ddzz_nav/src/gotogoal.py
@@ -53,8 +53,8 @@ class TurtleBot:
         print("goal received")
         goal_pose = self.pose_stamped_to_pose(goal_pose)
         print(goal_pose)
-        # self.move2goal(goal_pose)
-        self.move2angle(goal_pose.theta, 0.1)
+        self.move2goal(goal_pose)
+        # self.move2angle(goal_pose.theta, 0.1)
 
     def update_pose(self):
         """get pose from tf using get_pose()"""
@@ -73,17 +73,26 @@ class TurtleBot:
     def move2angle(self, angle, tolerance):
         """go to angle in radians"""
         self.update_pose()
+        # print("angle: " + str(angle))
+        # print("pose.theta: " + str(self.pose.theta))
+        # print("angle error: " + str(self.get_angle(self.pose, angle)))
         while abs(self.get_angle(self.pose, angle)) > tolerance:
             self.cmd_rotation(angle)
             # print("angle: " + str(angle))
             # print("pose.theta: " + str(self.pose.theta))
-            # print("diff: " + str(angle - self.pose.theta))
-            print()
+            # print("diff: " + str(self.get_angle(self.pose, angle)))
+            # print()
         
         # lock position for 1 second
         t = time.time()
         while time.time() - t < 1.0:
             self.cmd_rotation(angle)
+
+        # stop
+        # vel_msg = Twist()
+        # vel_msg.linear.x = 0
+        # vel_msg.angular.z = 0
+        # self.velocity_publisher.publish(vel_msg)
         
     
     def cmd_rotation(self, angle):
@@ -91,7 +100,7 @@ class TurtleBot:
         self.update_pose()
         vel_msg = Twist()
         vel_msg.linear.x = 0.0
-        vel_msg.angular.z = self.angular_vel(angle)
+        vel_msg.angular.z = self.angular_vel_rotation(angle)
         self.velocity_publisher.publish(vel_msg)
         self.rate.sleep()
         
@@ -100,9 +109,12 @@ class TurtleBot:
         return sqrt(pow((goal_pose.x - self.pose.x), 2) +
                     pow((goal_pose.y - self.pose.y), 2))
 
-    def linear_vel(self, goal_pose, constant=1.0):
+    def linear_vel(self, goal_pose, constant=2.0):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * self.euclidean_distance(goal_pose)
+        cmd = constant * self.euclidean_distance(goal_pose)
+        if abs(cmd) > self.max_vel:
+            cmd = self.max_vel * self.sign(cmd)
+        return cmd
 
     def steering_angle(self, goal_pose):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
@@ -110,8 +122,8 @@ class TurtleBot:
 
     def angular_vel(self, goal_pose, constant=2.0):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        print(self.steering_angle(goal_pose))
-        print(self.pose.theta)
+        # print(self.steering_angle(goal_pose))
+        # print(self.pose.theta)
         return constant * (self.steering_angle(goal_pose) - self.pose.theta)
     
     def get_angle(self, start_pose, goal_pose):
@@ -131,12 +143,12 @@ class TurtleBot:
         return angle
 
 
-    def angular_vel(self, goal_angle, constant=2.5):
-        print("goal_angle: " + str(goal_angle))
-        print("pose.theta: " + str(self.pose.theta))
-        print("diff: " + str(goal_angle - self.pose.theta))
+    def angular_vel_rotation(self, goal_angle, constant=2.5):
+        # print("goal_angle: " + str(goal_angle))
+        # print("pose.theta: " + str(self.pose.theta))
+        # print("diff: " + str(goal_angle - self.pose.theta))
         cmd = self.get_angle(self.pose, goal_angle)
-        print("cmd: " + str(cmd))
+        # print("cmd: " + str(cmd))
         cmd = constant * cmd
         if abs(cmd) > self.max_ang_vel:
             cmd =  self.max_ang_vel * self.sign(cmd)
@@ -180,6 +192,19 @@ class TurtleBot:
         
         dist = 10000.0
 
+        self.update_pose()
+
+        print("pose :")
+        print("x: ", self.pose.x)
+        print("y: ", self.pose.y)
+        print("theta: ", self.pose.theta)
+        print("distance to goal: ", dist)
+        print()
+
+        self.move2angle(self.steering_angle(goal_pose), 0.05)
+
+        print("moved to angle")
+
         while dist >= distance_tolerance:
 
             dist = self.euclidean_distance(goal_pose)
@@ -189,16 +214,10 @@ class TurtleBot:
 
             self.update_pose()
 
-            print("pose :")
-            print("x: ", self.pose.x)
-            print("y: ", self.pose.y)
-            print("theta: ", self.pose.theta)
-            print("distance to goal: ", dist)
-            print()
+
             # Linear velocity in the x-axis.
             vel_msg.linear.x = self.linear_vel(goal_pose)
-            if abs(vel_msg.linear.x) > self.max_vel:
-                vel_msg.linear.x = self.max_vel
+
             vel_msg.linear.y = 0
             vel_msg.linear.z = 0
 
@@ -206,15 +225,18 @@ class TurtleBot:
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
             vel_msg.angular.z = self.angular_vel(goal_pose)
-            if abs(vel_msg.angular.z) > self.max_ang_vel:
-                vel_msg.angular.z = self.max_ang_vel
+
+            # disable linear velocity if angular velocity is too high
             if abs(vel_msg.angular.z) > self.ang_vel_disabling_lin_vel:
                 vel_msg.linear.x = 0.0
+                print("angular velocity too high, disabling linear velocity")
+            else:
+                print("angular velocity low enough, enabling linear velocity")
         
-            print("vel_msg :")
-            print("linear.x: ", vel_msg.linear.x)
-            print("angular.z: ", vel_msg.angular.z)
-            print()
+            # print("vel_msg :")
+            # print("linear.x: ", vel_msg.linear.x)
+            # print("angular.z: ", vel_msg.angular.z)
+            # print()
 
 
             # Publishing our vel_msg
@@ -228,8 +250,22 @@ class TurtleBot:
         vel_msg.angular.z = 0
         self.velocity_publisher.publish(vel_msg)
 
+        print("moved to goal")
+        print("pose :")
+        print("x: ", self.pose.x)
+        print("y: ", self.pose.y)
+        print("theta: ", self.pose.theta)
+        print("distance to goal: ", dist)
+        print()
+
+        self.move2angle(goal_pose.theta, 0.05)
+
+        print("moved to angle")
+
         # # If we press control + C, the node will stop.
         # rospy.spin()
+
+        print("goal reached")
 
 
 if __name__ == '__main__':
